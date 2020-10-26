d9552c357605b49a1877d444ebf90e2f1dba7874
parihaaraka <lukyanov@kickico.com>
2020-10-26 16:09:37 +0300

    extern C wrapper

--- ext_tnt.h
+++ ext_tnt.h
@@ -5,9 +5,17 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
 int
 mp_fprint_ext_tnt(FILE *file, const char **data, int depth);
 int
 mp_snprint_ext_tnt(char *buf, int size, const char **data, int depth);
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
 
 #endif /* EXT_TNT_H_INCLUDED */
